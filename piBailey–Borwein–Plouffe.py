f03a78cd3dd023ee427927e2b4e3e2695ecee951
lolerwaffles <walterscottwilliams@hotmail.com>
2018-01-16 11:36:15 -0600

    pi calc

--- piBailey–Borwein–Plouffe.py
+++ piBailey–Borwein–Plouffe.py
@@ -4,18 +4,23 @@ import math
 
 '''This program calculates pi to a user specified number of decimal places
 using the Bailey-Borwein-Plouffe formula'''
-print("How Many decimal places do you want to calculate pi too?")
-number_of_decimal_places = int(input())
+print("This program calculates pi to a user specified number of decimal places
+using the Bailey-Borwein-Plouffe formula")
+number_of_decimal_places = int(input("How Many decimal places do you want to calculate pi too?"))
 getcontext().prec = number_of_decimal_places
 i = 0
 pie = Decimal(0)
 prev_pie = 1
 
-while pie != prev_pie:
-    prev_pie = pie
-    pie += (Decimal(1)/(16**i))*((Decimal(4)/(8*i+1))-(Decimal(2)/(8*i+4))-(Decimal(1)/(8*i+5))-(Decimal(1)/(8*i+6)))
-    print(Decimal(pie))
-    i += 1
+while True:
+    if pie == prev_pie:
+        break
+    else:
+        prev_pie = pie
+        pie += (Decimal(1)/(16**i))*((Decimal(4)/(8*i+1))-(Decimal(2)/(8*i+4))-(Decimal(1)/(8*i+5))-(Decimal(1)/(8*i+6)))
+        print(Decimal(pie))
+        i += 1
+
 
 error = abs(Decimal(math.pi) - pie)
 
